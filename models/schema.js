e0d64eea6f8c563aa01471f3244b7c0f463ae2de
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2023-12-21 20:02:35 +0200

    up schema

--- models/schema.js
+++ models/schema.js
@@ -4,15 +4,15 @@ const contactSchema = Joi.object({
   name: Joi.string()
     .min(3)
     .max(30)
-    .required(),
-    // .messages({ "any.required": "missing required name field" }),
+    .required()
+    .messages({ "any.required": "missing required name field" }),
   email: Joi.string()
     .email()
-    .required(),
-    // .messages({ "any.required": "missing required email field" }),
+    .required()
+    .messages({ "any.required": "missing required email field" }),
   phone: Joi.string()
-    .required(),
-    // .messages({ "any.required": "missing required phone field" }),
+    .required()
+    .messages({ "any.required": "missing required phone field" }),
 });
 
 
