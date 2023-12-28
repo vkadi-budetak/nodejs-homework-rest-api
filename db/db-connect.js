07b78c58514337457f2ff489b49a85be47292a01
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2023-12-28 23:52:04 +0200

    update

---
+++ db/db-connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const { DB_HOST } = process.env
+
+exports.dbConnect = async () => {
+  try {
+    await mongoose.connect(DB_HOST);
+    console.log('Database connection successful');
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,26 @@
 const express = require('express')
+const { validateBody, isValidId } = require("../../Middleware");
+const {
+  addSchema,
+  updateSchema,
+  updateStatusSchema,
+} = require("../../models/contact");
+
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../models/contact");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.listContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateBody(addSchema), ctrl.addContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", isValidId, ctrl.removeContact);
+
+router.put("/:contactId", isValidId, validateBody(updateSchema), ctrl.updateContact);
+
+router.patch("/:contactId/favorite",isValidId,validateBody(updateStatusSchema),ctrl.updateStatusContact);
 
 module.exports = router
