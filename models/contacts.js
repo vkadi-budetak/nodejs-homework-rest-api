f99980c93c8b003aafcbba8dab1bc7bd81275809
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2024-01-02 21:33:49 +0200

    exampl

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,85 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const { Contact } = require("./schema");
+const { HttpError, ctrlWrapper } = require("../helpers");
 
-const getContactById = async (contactId) => {}
+// ============================== Get All
 
-const removeContact = async (contactId) => {}
+const listContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
 
-const addContact = async (body) => {}
+// ============================== Get by ID
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findOne({ _id: contactId });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+// ============================== Add
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+
+  res.status(201).json(result);
+};
+
+// ============================== Delete
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  console.log(Contact);
+  const result = await Contact.findByIdAndDelete({ _id: contactId });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json({ message: "contact deleted" });
+};
+
+// ============================== Update
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate({ _id: contactId }, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+// ============================== Update status
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate({ _id: contactId }, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const { dbConnect } = require("./db/db-connect");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { PORT } = process.env || 8000;
+
+const startServer = async () => {
+  try {
+    await dbConnect();
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
